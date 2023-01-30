dcdd46ff4e668ea12546eb90f726652882b51bec
Pedro Vega <pedrovega468@yahoo.com>
2023-01-30 18:35:13 -0300

    Implement login, index, viewProduct, settings

---
+++ src/main/resources/static/web/assets/js/index.js
@@ -0,0 +1,25 @@
+
+const {createApp} = Vue
+
+const index = createApp({
+    data(){
+        return {
+           login: true, 
+         
+        }
+    },
+    created(){
+        
+        
+    },
+    methods: {
+     
+            
+    },
+    computed: {
+
+    }
+})
+
+
+index.mount('#index')
---
+++ src/main/resources/static/web/assets/js/settings.js
@@ -0,0 +1,25 @@
+
+const {createApp} = Vue
+
+const settings = createApp({
+    data(){
+        return {
+           login: true, 
+         
+        }
+    },
+    created(){
+        
+        
+    },
+    methods: {
+     
+            
+    },
+    computed: {
+
+    }
+})
+
+
+settings.mount('#settings')
