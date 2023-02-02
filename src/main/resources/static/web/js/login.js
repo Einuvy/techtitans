2055bc17e2b0e4e6e4d5cde9cdab720da10e92b0
FelipeGalleguillos <f.i.galleguillos@gmail.com>
2023-02-02 02:12:18 -0300

    implementacion de login.js

--- src/main/resources/static/web/js/login.js
+++ src/main/resources/static/web/js/login.js
@@ -1,24 +1,60 @@
-const {createApp} = Vue
+const { createApp } = Vue
 
 const login = createApp({
-    data(){
+    data() {
         return {
-           login: true, 
-           
-
-
-            
+            login: true,
+            firstName:"",
+            lastName:"",
+            email:"",
+            password:"",
+            address:"",
+            birthDate:"",
+            phoneNumber:"",
+            msg:""
         }
     },
     created(){
-        
-        
+
     },
-    methods: {
-     
-            
+    methods:{
+        signUp(){
+            let aux = this.birthDate.split("-")
+            let date = aux[2]+"/"+aux[1]+"/"+aux[0]
+            axios.post('/api/customers',{firstName:this.firstName,lastName:this.lastName,email:this.email,password:this.password,address:this.address,birthDate:date,phoneNumber:this.phoneNumber})
+            .then(res=>{
+                console.log(res)
+                axios.post('/api/login',`email=${this.email}&password=${this.password}`)
+                .then(res=>{
+                    console.log(res)
+                    window.location.href="/web/products.html"
+                })
+                .catch(err=>console.log(err))
+            })
+            .catch(err=>{
+                console.log(err)
+                this.msg=err.response.data
+                setTimeout(() => {this.msg=''}, 2000);
+            })
+        },
+        logIn(){
+            axios.post('/api/login',`email=${this.email}&password=${this.password}`)
+            .then(res=>{
+                console.log(res)
+                if (this.email.includes("@admin")) {
+                    window.location.href="/admin.html"
+                }else{
+                    window.location.href="/web/products.html"
+                }
+            })
+            .catch(err=>{
+                console.log(err)
+                this.msg=err.response.data.error
+                setTimeout(() => {this.msg=''}, 2000);
+            })
+        }
     },
-    computed: {
+    computed:{
 
     }
 })
