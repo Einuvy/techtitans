0e26539d017e1b346274efdf2a9a12970afcaa35
Pedro Vega <pedrovega468@yahoo.com>
2023-02-01 23:54:19 -0300

    lastChanges

--- src/main/resources/static/web/js/index.js
+++ src/main/resources/static/web/js/index.js
@@ -5,18 +5,51 @@ const index = createApp({
     data(){
         return {
            login: true, 
-         
+           products: [],
+           productsFilter: [],
+           searchInput: "",
+           select: "Category",
+           categories: [],
+           productCart: []
         }
     },
     created(){
         
+        this.loadData()
         
+        if (localStorage.getItem("cart")) {
+            this.productCart = JSON.parse(localStorage.getItem('cart'))
+        }
     },
     methods: {
         
         loadData() {
-            
-        }
+            axios.get("/api/products")
+            .then(res => {
+              this.products = res.data
+              this.productsFilter = this.products
+              console.log(this.products);
+            }).catch(error => console.error(error))
+        },
+        addCart(product) {
+            let alreadyInCart = this.productCart.find((item) => item.id === product.id)
+            if (alreadyInCart) {
+                alert("Product already in cart");
+            } else {
+                this.productCart.push(product);
+                this.saveCartToLocalStorage()
+            }
+
+        },
+        removeFromCart(product) {
+            const productIndex = this.productCart.findIndex(p => p.id === product.id);
+            if (productIndex === -1) return
+            this.productCart.splice(productIndex, 1)
+            this.saveCartToLocalStorage()
+        },
+        saveCartToLocalStorage() {
+            localStorage.setItem("cart", JSON.stringify(this.productCart))
+        },
             
     },
     computed: {
