164a633b5b79811856576a27f2ae1fabf3d8e3e0
Elias Nicolas Medina Villalba <eliasnikolasmv@gmail.com>
2023-02-02 14:04:34 -0300

    Product Servlet added and softdelete added

--- src/main/java/com/techtitans/ecommerce/services/implementations/ProductServiceImplementation.java
+++ src/main/java/com/techtitans/ecommerce/services/implementations/ProductServiceImplementation.java
@@ -3,25 +3,37 @@ package com.techtitans.ecommerce.services.implementations;
 import com.techtitans.ecommerce.models.Product;
 import com.techtitans.ecommerce.repositories.ProductRepository;
 import com.techtitans.ecommerce.services.ProductService;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
 public class ProductServiceImplementation implements ProductService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     ProductRepository productRepository;
 
     @Override
     public List<Product> getAllProducts() {
-        return productRepository.findAll();
+        entityManager.unwrap(Session.class).enableFilter("deletedProduct").setParameter("deleted", false);
+        List<Product> products = productRepository.findAll();
+        entityManager.unwrap(Session.class).disableFilter("deletedProduct");
+        return products;
     }
 
     @Override
     public Product findProductById(Long id) {
-        return productRepository.findById(id).orElse(null);
+        entityManager.unwrap(Session.class).enableFilter("deletedProduct").setParameter("deleted", false);
+        Product product = productRepository.findById(id).orElse(null);
+        entityManager.unwrap(Session.class).disableFilter("deletedProduct");
+        return product;
     }
 
     @Override
